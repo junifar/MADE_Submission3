ccbb557bc5d283bca731bf20b73e2d9fdeb7d585
Junifar Hidayat <junifar@gmail.com>
2019-07-03 14:15:04 +0700

    livemodel implementation

---
+++ app/src/main/java/com/rubahapi/moviedb/main/fragment/movie/MoviewFragmentViewModel.kt
@@ -0,0 +1,24 @@
+package com.rubahapi.moviedb.main.fragment.movie
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.rubahapi.moviedb.model.Movie
+
+class MoviewFragmentViewModel: ViewModel(){
+
+    private val items: MutableLiveData<List<Movie>> by lazy {
+        MutableLiveData().also{
+            loadMovie()
+        }
+    }
+
+    fun getMovies(): LiveData<List<Movie>>{
+        return items
+    }
+
+    private fun loadMovie(){
+
+    }
+
+}
